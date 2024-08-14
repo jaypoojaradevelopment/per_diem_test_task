95bde9ae022980eb7068376185bbc17b12bd90b0
jaypoojaradevelopment <jaypoojara.development@gmail.com>
2024-08-14 16:08:32 +0530

    added navigation

---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+import AuthScreen from './Auth';
+import HomeScreen from './Home';
+import OnBoardingScreen from './OnBoarding';
+
+export {AuthScreen, HomeScreen, OnBoardingScreen};
---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  green: '#02AA15',
+  yellow: '#F3FF00',
+  red: '#FF0000',
+  primary: '#694F8E',
+  primaryLight: '#AC97C9',
+  btnColor: '#FCA851',
+  white: '#fff',
+  black: '#000',
+  transparent: 'transparent',
+  gray: '#A2A2A2',
+};
+
+export {colors};
